97523333de3dfb7f383e509f1b2c96caa5352a8b
winkidney <winkidney@gmail.com>
2015-08-01 14:52:04 +0800

    update schema and example
    1. update schema style
    2. add custom method call support
    3. flask url map
    4. update example

--- examples/cmd_app.py
+++ examples/cmd_app.py
@@ -7,18 +7,24 @@ jar = FlaskJar(__name__, flask_app)
 
 cmd = jar.entry_point
 
-@jar.register
-class ExampleSugar(FlaskSugar):
+bl = Blueprint("api", __name__, url_prefix="/api")
+
+@jar.register(blue_print=bl)
+class DiskSugar(FlaskSugar):
     config = {
         "schema": {
-            "help": "command to show how this api-maker works",
-            "type": "object",
-            "properties":{
-                "price": {"type": "number"},
-                "name": {"type": "string"}
-            }
+            "create": {
+                "help": "command to show how this api-maker works",
+                "type": "object",
+                "properties":{
+                    "name": {"type": "string"},
+                    "size": {"type": "string"},
+                },
+                "required": ["username", "password"]
+            },
+            "support_operations": ('create', "show", "delete"),
         },
-        "url": "/pools",
+        "url": "/disk/<int:disk_id>",
         "version": "1"
     }
 
@@ -26,34 +32,7 @@ class ExampleSugar(FlaskSugar):
         print("Api running got: %s" % result)
         return result
 
-    def show(self, data, web_request, **kwargs):
-        return {
-            "pools": [
-                {'name': "pool1"}
-            ],
-        }
-
-bl = Blueprint("api", __name__, url_prefix="/api")
-
-@jar.register(blue_print=bl)
-class DiskSugar(FlaskSugar):
-    config = {
-        "schema": {
-            "help": "show disk list and create disk, etc",
-            "type": "object",
-            "properties":{
-                "pool_id": {"type": "number"},
-            }
-        },
-        "url": "/disks/<int:disk_id>",
-        "version": "1",
-    }
-
-    def cli_response(self, result, **kwargs):
-        print("Api running got: %s" % result)
-        return result
-
-    def show(self, data, web_request, disk_id):
+    def show(self, data, web_request, disk_id, **kwargs):
         return {
             "disks": [
                 {'name': "disk.%s" % disk_id}
@@ -62,7 +41,7 @@ class DiskSugar(FlaskSugar):
 
 def run_server():
     flask_app.register_blueprint(bl)
-    flask_app.run(debug=True, host="0.0.0.0")
+    flask_app.run(debug=True, host="0.0.0.0", port=7001)
 
 if __name__ == "__main__":
     run_server()
--- src/schema_sugar/contrib/flask_sugar.py
+++ src/schema_sugar/contrib/flask_sugar.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
-from functools import wraps
-from flask import request, jsonify, Blueprint
+from flask import request, jsonify, Blueprint, url_for
 from schema_sugar import SugarJarBase, SchemaSugarBase
 
 __all__ = (
@@ -10,7 +9,7 @@ __all__ = (
 
 class FlaskSugar(SchemaSugarBase):
 
-    def make_resource(self, decorators=()):
+    def make_resource(self, decorators=None):
         """
         register resource to flask, if blue_print is not None, it will be
          registered to blue_print, else the app.
@@ -21,18 +20,20 @@ class FlaskSugar(SchemaSugarBase):
         """
 
         def resource(*args, **kwargs):
-            if request.method == "POST":
+            if request.method in ("POST", "PUT"):
                 data = request.get_json(force=True)
             else:
                 data = request.args
             return self._api_run(data, request.method, web_request=request, **kwargs)
-
-        for decorator in decorators:
-            resource = decorator(resource)
+        if decorators is not None:
+            for decorator in decorators:
+                resource = decorator(resource)
         return resource
 
     def web_response(self, result, http_code=200):
         return jsonify(result), http_code
+
+
 
 
 class FlaskJar(SugarJarBase):
@@ -43,13 +44,19 @@ class FlaskJar(SugarJarBase):
         super(FlaskJar, self).__init__(name)
         self.registry = set()
         self.app = flask_app
+        self.app.add_url_rule("/meta", endpoint="site_map", view_func=self.site_map)
 
     def run(self, *args, **kwags):
         self.app.run(*args, **kwags)
 
-    def register(self, schema_sugar_class=None, blue_print=None, args=(), kwargs={}):
+    def register(self, schema_sugar_class=None, blue_print=None, decorators=None, args=(), kwargs={}):
         """
+        :param args: args passed to schema_sugar_class
+        :param kwargs: kwargs passed to schema_sugar_class
+        :type args: list or tuple
+        :type kwargs: dict
         :type schema_sugar_class: schema_sugar.contrib.flask_sugar.FlaskSugar
+        :type decorators: a list of flask view decorator
         """
         if schema_sugar_class is not None and not issubclass(schema_sugar_class, FlaskSugar):
             raise TypeError(
@@ -61,12 +68,16 @@ class FlaskJar(SugarJarBase):
 
         if blue_print or args or kwargs:
             def wrapper(schema_class):
-                return self._register(schema_class(*args, **kwargs), blue_print=blue_print)
+                return self._register(
+                    schema_class(*args, **kwargs),
+                    blue_print=blue_print,
+                    decorators=decorators,
+                )
             return wrapper
         else:
-            return self._register(schema_sugar_instance=schema_sugar_class())
+            return self._register(schema_sugar_class(), decorators=decorators)
 
-    def _register(self, schema_sugar_instance, blue_print=None):
+    def _register(self, schema_sugar_instance, blue_print=None, decorators=None):
         """
         :type schema_sugar_instance: schema_sugar.contrib.flask_sugar.FlaskSugar
         """
@@ -74,7 +85,7 @@ class FlaskJar(SugarJarBase):
         self.registry.add(schema_sugar)
         schema_sugar.make_cli(self.entry_point)
 
-        resource = schema_sugar.make_resource()
+        resource = schema_sugar.make_resource(decorators=decorators)
         if blue_print is not None:
             route_proxy = blue_print
         else:
@@ -88,3 +99,19 @@ class FlaskJar(SugarJarBase):
             schema_sugar.url + "/meta", endpoint=schema_sugar.url+"-doc",
             view_func=schema_sugar.get_doc, methods=("GET", ),
         )
+        return resource
+
+    def has_no_empty_params(self, rule):
+        defaults = rule.defaults if rule.defaults is not None else ()
+        arguments = rule.arguments if rule.arguments is not None else ()
+        return len(defaults) >= len(arguments)
+
+    def site_map(self):
+        links = []
+        for rule in self.app.url_map.iter_rules():
+            if "GET" in rule.methods and self.has_no_empty_params(rule):
+                url = url_for(rule.endpoint)
+                links.append((url, rule.methods, rule.endpoint))
+        return "All api listed by url and name, " + \
+            "view them with postfix `meta/`:<br>" + \
+            "<br>".join(str(x) for x in links)
